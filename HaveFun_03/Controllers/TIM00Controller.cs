8d7291467eed7ce38020c691fa3767f007c3f4f7
iven <iven840320@gmail.com>
2024-05-29 16:07:22 +0800

    add0

---
+++ HaveFun_03/Controllers/TIM00Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun_03.Controllers
+{
+    public class TIM00Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
